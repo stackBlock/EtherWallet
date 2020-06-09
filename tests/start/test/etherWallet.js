82f508ff259694d2dd84185a50cc8d108e30c788
stackblock <stackchain@gmail.com>
2020-06-09 13:57:57 -0400

    tests work!!

--- tests/start/test/etherWallet.js
+++ tests/start/test/etherWallet.js
@@ -1,8 +1,52 @@
-const EtherWallet = artifacts.require('EtherWallet');
+const EtherWallet = artifacts.require("EtherWallet");
 
-contract('EtherWallet', () => {
+contract("EtherWallet", (accounts) => {
   let etherWallet = null;
   before(async () => {
     etherWallet = await EtherWallet.deployed();
+  });
+
+  it("Should set accounts[0] as owner", async () => {
+    const owner = await etherWallet.owner();
+    assert(owner === accounts[0]);
+  });
+
+  it("Should deposit ether to the ether wallet", async () => {
+    await etherWallet.deposit({
+      from: accounts[0], // will use accounts[0] if not specified anyway
+      value: 100,
+    });
+    const balance = await web3.eth.getBalance(etherWallet.address);
+    assert(parseInt(balance) === 100);
+  });
+
+  it("Should return the balance of the contract", async () => {
+    const balance = await etherWallet.balanceOf();
+    assert(parseInt(balance) === 100);
+  });
+
+  it("Should transfer ether to another address", async () => {
+    const balanceRecipientBefore = await web3.eth.getBalance(accounts[1]);
+    await etherWallet.send(accounts[1], 50, {
+      from: accounts[0],
+    });
+    const balanceWallet = await web3.eth.getBalance(etherWallet.address);
+    assert(parseInt(balanceWallet) === 50);
+    const balanceRecipientAfter = await web3.eth.getBalance(accounts[1]);
+    const initialBalance = web3.utils.toBN(balanceRecipientBefore);
+    const finalBalance = web3.utils.toBN(balanceRecipientAfter);
+    assert(finalBalance.sub(initialBalance).toNumber() === 50);
+  });
+
+  it("Should not transfer ether if tx not sent from owner", async () => {
+    try {
+      await etherWallet.send(accounts[1], 50, {
+        from: accounts[2],
+      });
+    } catch (e) {
+      assert(e.message.includes("sender is not allowed"));
+      return;
+    }
+    assert(false);
   });
 });
